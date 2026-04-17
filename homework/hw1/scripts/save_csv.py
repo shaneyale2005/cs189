7c0daa10522ca5324281099a829dab59f7fb9a1d
shaneyale <shaneyale86@gmail.com>
2026-04-17 11:42:41 +0800

    remove unused scripts and old lecture materials; add new lecture PDFs

--- homework/hw1/scripts/save_csv.py
+++
@@ -1,9 +0,0 @@
-# A code snippet to help you save your results into a kaggle accepted csv
-import pandas as pd
-import numpy as np
-
-def results_to_csv(y_test, file_name):
-    y_test = y_test.astype(int)
-    df = pd.DataFrame({'Category': y_test})
-    df.index += 1
-    df.to_csv(file_name, index_label='Id')
